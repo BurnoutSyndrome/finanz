e47fcf7090e9fa8911385a48d99ce80235b1ded0
Selçuk Öztürk <selcuk@hotmail.com.tr>
2017-03-28 14:48:22 +0300

    Added first source.

---
+++ src/main/java/com/bs/finanz/FinanzApplication.java
@@ -0,0 +1,12 @@
+package com.bs.finanz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzApplication.class, args);
+	}
+}
