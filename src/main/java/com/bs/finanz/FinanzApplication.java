52daeec4e276b04f007e4934effe5ccd7b9679e1
Selçuk Öztürk <selcukahmed@gmail.com>
2017-04-02 00:58:58 +0300

    Added swagger.

--- src/main/java/com/bs/finanz/FinanzApplication.java
+++ src/main/java/com/bs/finanz/FinanzApplication.java
@@ -2,8 +2,10 @@ package com.bs.finanz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FinanzApplication {
 
     public static void main(String[] args) {
