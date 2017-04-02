696d53f1f5747c67c58ba7f1caa1d9f7435403aa
Selçuk Öztürk <selcukahmed@gmail.com>
2017-04-02 23:21:43 +0300

    Added swagger as index.

---
+++ src/main/java/com/bs/finanz/controllers/Index.java
@@ -0,0 +1,16 @@
+package com.bs.finanz.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+    @Value("${url.swagger}")
+    private String swaggerUrl;
+
+    @RequestMapping("/")
+    public String home() {
+        return String.format("forward:/%s", swaggerUrl);
+    }
+}
